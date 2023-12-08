e1ae723f9ba9d7b1e9645f174b69815666ef822b
이호진 <gommind@naver.com>
2023-12-08 10:41:25 +0900

    어댑터 인바운드 아웃바운드 모양 예시

---
+++ src/main/java/com/example/hexagonaltest/board/adapter/in/web/BoardController.java
@@ -0,0 +1,8 @@
+package com.example.hexagonaltest.board.adapter.in.web;
+
+/**
+ * 설명 : BoardController
+ *       -> int 바운드 어댑터
+ */
+public class BoardController {
+}
---
+++ src/main/java/com/example/hexagonaltest/board/adapter/out/persistence/JPABoardOutputAdapter.java
@@ -0,0 +1,9 @@
+package com.example.hexagonaltest.board.adapter.out.persistence;
+
+/**
+ * 설명 : JPABoardOutputAdapter
+ *       -> 레이아웃에서
+ *          DAOImpl의 역할이다.
+ */
+public class JPABoardOutputAdapter {
+}
